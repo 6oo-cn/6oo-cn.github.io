dee22b2732b28221acd7aa40c5ef54363e667ae7
6-js <vip.6oo@qq.com>
2024-02-18 14:42:27 +0800

    MyFirstCommit

---
+++ en/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/en/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "English documentation is not available on this site\n",
+    "description": "",
+    "tags": null,
+    "title": "Site description",
+    "uri": "/en/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/en/tags/index.html"
+  }
+]
---
+++ pir/index.search.js
@@ -0,0 +1,26 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/pir/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": " Arrr! Pirrrates Fello' pirrrates, grog made us dizzy! Be awarrre some stuff may look weird in this trrranslat'n. Like Merrrmaids, do'n math or chemistrrry and stuff.\nEnglish documentat'n be not avail'ble on this ship\n",
+    "description": "",
+    "tags": null,
+    "title": "Site description",
+    "uri": "/pir/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/pir/tags/index.html"
+  }
+]
